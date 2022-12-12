0cd438b656fc6567aeabba42f2252cd5a5fff738
Jasem Duncan <jasemvalencia@gmail.com>
2022-12-12 16:01:15 -0500

    implement Erase function

--- 06-proyecto1/src/components/Listado.js
+++ 06-proyecto1/src/components/Listado.js
@@ -13,7 +13,25 @@ export const Listado = ({listadoState, setListadoState}) => {
         let peliculas = JSON.parse(localStorage.getItem("pelis"));
         console.log(peliculas);
         setListadoState(peliculas);
+        return peliculas;
     };
+
+    const borrarPeli = (id)=> {
+        // alert(id)
+        //Conseguir peliculas almacenadas
+        let pelis_almacenadas = conseguirPeliculas();
+
+        //Filtrar esas peliculas para que elimine del array la que no quiero
+        let nuevo_array_pelis = pelis_almacenadas.filter(peli =>peli.id!== parseInt(id));
+
+        //Actualizar estado del listado
+        console.log(pelis_almacenadas, nuevo_array_pelis);
+
+        setListadoState(nuevo_array_pelis);
+
+        //Actualizar datos en el local storage
+        localStorage.setItem('pelis',JSON.stringify(nuevo_array_pelis));
+    }
 
     return (
         <>
@@ -23,7 +41,7 @@ export const Listado = ({listadoState, setListadoState}) => {
                         <h3 className="title">{peli.titulo}</h3>
                         <p className="description">{peli.descripcion}</p>
                         <button className="edit">Editar</button>
-                        <button className="delete">Borrar</button>
+                        <button className="delete" onClick={()=> borrarPeli(peli.id)}>Borrar</button>
                     </article>
                 );
             })
