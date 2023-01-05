ba07fcedce626f108fc6af7c30fcce0711dd0777
Jasem Duncan <jasemvalencia@gmail.com>
2023-01-04 23:54:32 -0500

    create routes and components

---
+++ 07-rutas/src/components/Articulos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Articulos = () => {
+  return (
+    <div>
+        <h1>Pagina de articulos</h1>
+        <p>Contenido pagina de articulos</p>
+    </div>
+  )
+}
---
+++ 07-rutas/src/components/Contacto.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Contacto = () => {
+  return (
+    <div>
+        <h1>Pagina de contacto</h1>
+        <p>Contenido pagina de contacto</p>
+    </div>
+ )
+}
