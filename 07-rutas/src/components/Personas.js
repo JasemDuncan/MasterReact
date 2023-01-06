78952863b646611c0155015083fc200e653f7452
Jasem Duncan <jasemvalencia@gmail.com>
2023-01-05 23:51:08 -0500

    create new proyect for portafolio

--- 07-rutas/src/components/Personas.js
+++ 07-rutas/src/components/Personas.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 export const Personas = () => {
 
@@ -15,11 +15,36 @@ export const Personas = () => {
 
 
     const {nombre, apellido}=useParams();
+    const navegar = useNavigate();
+
+    const enviar=(e)=>{
+        e.preventDefault();
+        let nombre =e.target.nombre.value;
+        let apellido = e.target.apellido.value;
+        let url =`/persona/${nombre}/${apellido}`;
+
+        if(nombre.length <=0 && apellido.length<=0) {
+            
+            navegar("/inicio");
+        }else if(nombre=="contacto"){
+            navegar("/contacto");
+        }else{
+            navegar(url);
+        }
+        
+    }
+
     return (
         <div>
             {!nombre && <h1>No hay ninguna person que mostrar.</h1>}
             {nombre && <h1>Pagina de persona: {nombre+" "+apellido}</h1>}            
             <p>Esta es la pagina de persona</p>
+
+            <form onSubmit={enviar}>
+                <input type="text" name="nombre" />
+                <input type="text" name="apellido" />
+                <input type="submit" name="enviar" value ="enviar"/>
+            </form>
         </div>
     )
 }
--- 07-rutas/src/routers/RouterPrincipal.js
+++ 07-rutas/src/routers/RouterPrincipal.js
@@ -5,6 +5,13 @@ import { Inicio } from '../components/Inicio';
 import { Contacto } from '../components/Contacto';
 import { Error } from '../components/Error';
 import { Personas } from '../components/Personas';
+import { PanelControl } from '../components/PanelControl';
+import { Inicio as InicioPanel } from '../components/panel/Inicio'
+import { Crear } from '../components/panel/Crear';
+import { Gestion } from '../components/panel/Gestion';
+import { Acerca } from '../components/panel/Acerca';
+
+
 
 export const RouterPrincipal = () => {
   return (
@@ -15,20 +22,26 @@ export const RouterPrincipal = () => {
           <li>
             <NavLink
               to="/inicio"
-              className={({isActive}) => isActive ? "activado" : ""}
+              className={({ isActive }) => isActive ? "activado" : ""}
             >Inicio</NavLink>
           </li>
           <li>
-            <NavLink 
-            to="/articulos"
-            className={({isActive}) => isActive ? "activado" : ""}
+            <NavLink
+              to="/articulos"
+              className={({ isActive }) => isActive ? "activado" : ""}
             >Articulos</NavLink>
           </li>
           <li>
-            <NavLink 
-            to="/contacto"
-            className={({isActive}) => isActive ? "activado" : ""}
+            <NavLink
+              to="/contacto"
+              className={({ isActive }) => isActive ? "activado" : ""}
             >Contacto</NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/panel"
+              className={({ isActive }) => isActive ? "activado" : ""}
+            >Panel de control</NavLink>
           </li>
         </ul>
       </nav>
@@ -41,10 +54,17 @@ export const RouterPrincipal = () => {
           <Route path="/inicio" element={<Inicio />} />
           <Route path="/articulos" element={<Articulos />} />
           <Route path="/contacto" element={<Contacto />} />
-          <Route path='/persona/:nombre/:apellido' element={<Personas/>} />
-          <Route path='/persona/:nombre' element={<Personas/>}/>
-          <Route path='/persona' element={<Personas/>}/>
-          <Route path='/redirigir'element={<Navigate to={'/persona/jasem/valencia'}/>}/>
+          <Route path='/persona/:nombre/:apellido' element={<Personas />} />
+          <Route path='/persona/:nombre' element={<Personas />} />
+          <Route path='/persona' element={<Personas />} />
+          <Route path='/redirigir' element={<Navigate to={'/persona/jasem/valencia'} />} />
+          <Route path='/panel/*' element={<PanelControl />}>
+            <Route index element={<Inicio />} />
+            <Route path='inicio' element={<InicioPanel />} />
+            <Route path='crear-articulos' element={<Crear />} />
+            <Route path='gestion-articulos' element={<Gestion />} />
+            <Route path='acerca-de' element={<Acerca />} />
+          </Route>
           <Route path="*" element={<Error />} />
         </Routes>
       </section>
