e96782b88a7ea2ccead380debf64ef44f22342f6
Jasem Duncan <jasemvalencia@gmail.com>
2023-01-06 23:25:09 -0500

    create new components and routes

---
+++ 08-proyecto2/src/router/MisRutas.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Routes, Route, BrowserRouter, NavLink} from "react-router-dom"
+
+export const MisRutas = () => {    
+  return (
+    <BrowserRouter>
+        {/* */}
+    </BrowserRouter>
+  )
+}
