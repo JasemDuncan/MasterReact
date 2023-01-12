d998679727436d28cfcdce4d28b989a629aecccf
Jasem Duncan <jasemvalencia@gmail.com>
2023-01-11 23:42:37 -0500

    create List of proyects

--- 08-proyecto2/src/components/Inicio.js
+++ 08-proyecto2/src/components/Inicio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ListadoTrabajos } from './ListadoTrabajos'
 
 export const Inicio = () => {
   return (
@@ -9,9 +10,7 @@ export const Inicio = () => {
       <section className='lasts-works'>
         <h2 className='heading'>Algunos de mis proyectos</h2>
         <p>Estos son algunos de mis trabajos en desarrollo web</p>
-        <div className='works'>
-
-        </div>
+        <ListadoTrabajos />
       </section>
     </div>
   )
---
+++ 08-proyecto2/src/components/ListadoTrabajos.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { trabajos } from '../data/trabajos';
+
+export const ListadoTrabajos = () => {
+  return (
+   
+
+      <section className='works'>
+        {
+          trabajos.map(trabajo => {
+            return (
+              <article key={trabajo.id} className='work-item'>
+                <div className='mask'>
+                  <img src={"/images/" + trabajo.id + ".png"} />
+                </div>
+                <span>{trabajo.categorias}</span>
+                <h2><Link to={"/proyecto/" + trabajo.id}>{trabajo.nombre}</Link> </h2>
+                <h3>{trabajo.tecnologias}</h3>
+              </article>
+            )
+          })
+        }
+      </section>
+  )
+}
