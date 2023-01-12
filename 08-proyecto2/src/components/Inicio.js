384f84b5ce658969974d63c830b44d3a761685b8
Jasem Duncan <jasemvalencia@gmail.com>
2023-01-12 10:19:16 -0500

    finish portafolio

--- 08-proyecto2/src/components/Inicio.js
+++ 08-proyecto2/src/components/Inicio.js
@@ -6,11 +6,11 @@ export const Inicio = () => {
   return (
     <div className='home'>
       <h1>Hola, soy Jasem Valencia y soy <strong>Desarrollador Web</strong> en Peru, y ofrezco mis servicios de <strong>programacion y desarrollo</strong>  en todo tipo de proyectos web.</h1>
-      <h2>Te ayudo a crear tu sitio o aplicacion web, tener mas visibilidad y relevancia en internet. <Link to="/contacto">Contacta conmigo.</Link> </h2>
+      <h2 className='title'>Te ayudo a crear tu sitio o aplicacion web, tener mas visibilidad y relevancia en internet. <Link to="/contacto">Contacta conmigo.</Link> </h2>
       <section className='lasts-works'>
         <h2 className='heading'>Algunos de mis proyectos</h2>
         <p>Estos son algunos de mis trabajos en desarrollo web</p>
-        <ListadoTrabajos />
+        <ListadoTrabajos limite="2" />
       </section>
     </div>
   )
